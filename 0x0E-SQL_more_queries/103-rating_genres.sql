f5307f5ec2541ad75ac4484e08f45dff04c55a91
FoNiXPr020 <mohamedbarhoun1999@gmail.com>
2024-02-15 00:45:24 +0100

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,21 +1,17 @@
--- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
--- Records are ordered by descending rating.
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
-       INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;-- Lists all genres in the database hbtn_0d_tvshows_rate by their rating.
--- Records are ordered by descending rating.
-SELECT `name`, SUM(`rate`) AS `rating`
-  FROM `tv_genres` AS g
-       INNER JOIN `tv_show_genres` AS s
-       ON s.`genre_id` = g.`id`
-
-       INNER JOIN `tv_show_ratings` AS r
-       ON r.`show_id` = s.`show_id`
- GROUP BY `name`
- ORDER BY `rating` DESC;
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in descending order by their rating
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+-- cat 103-rating_genres.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows_rate
+SELECT
+g.name AS name,
+SUM(sr.rate) AS rating
+FROM
+tv_genres AS g
+JOIN tv_show_genres AS sg ON sg.genre_id = g.id
+JOIN tv_shows AS s ON sg.show_id = s.id
+JOIN tv_show_ratings AS sr ON s.id = sr.show_id
+GROUP BY name
+ORDER BY rating DESC, name
+;
